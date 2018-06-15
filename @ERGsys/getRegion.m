aeff8bb2371733187e9dc35405b22e7e4563aaec
acotorruelo <36445421+acotorruelo@users.noreply.github.com>
2018-06-15 19:14:55 +0200

    Delete getRegion.m

--- @ERGsys/getRegion.m
+++
@@ -1,6 +0,0 @@
-function out= getRegion(this)
-%GETREGION Returns the ERGregion or ERGmultiregion of an instance of
-%ERGsys.
-%   sys.GETREGION returns the ERGregion or ERGmultiregion of sys.
-out=this.region;
-end
